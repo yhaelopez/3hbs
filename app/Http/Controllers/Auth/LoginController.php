64c4ba9f828b5ea2385781504acc243e1a35549c
Yhael <yhaelopez@gmail.com>
2022-02-02 17:25:36 -0700

    passport login/logout configured, vuejs installation

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+
+        return response()->json([
+            'token'    => $request->user()->createToken($request->input('device_name'))->accessToken,
+            'user'     => $request->user()
+        ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        return $request->wantsJson()
+            ? new Response('', 204)
+            : redirect('/');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LoginController as ApiLoginControler;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['as' => 'api.'], function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('login', [ApiLoginControler::class, 'login'])->name('login');
+
+    Route::post('register', [RegisterController::class, 'register'])->name('register');
+
+    Route::group(['middleware' => ['auth:api']], function () {
+
+        Route::get('user', [AuthenticationController::class, 'user'])->name('user');
+
+        Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    });
 });
